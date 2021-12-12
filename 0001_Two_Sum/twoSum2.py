fabd35b866d9fc1ffda388017b5f88d28047edb0
masnic <47881310+masnic@users.noreply.github.com>
2021-12-12 17:46:54 -0500

    Create twoSum2.py

---
+++ 0001_Two_Sum/twoSum2.py
@@ -0,0 +1,13 @@
+def twoSum(nums,target):
+    dict = {}
+    for i in range(len(nums)):
+        dict.update({nums[i] : i})  #Generate the dictionary: each elements in [nums] as Key, and index as dictionay value
+    for j in range(len(nums)):
+        diff = target - nums[j]
+        if diff in dict and dict[diff] != j: # Remove the case of one number and its double as target
+            return [j, dict[diff]]
+        else:
+            return -1
+
+        
+      
