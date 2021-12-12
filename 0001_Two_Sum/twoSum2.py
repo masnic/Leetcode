524111a05827ba575f214eed4996769729bd1e18
masnic <47881310+masnic@users.noreply.github.com>
2021-12-12 17:52:41 -0500

    Update twoSum2.py

--- 0001_Two_Sum/twoSum2.py
+++ 0001_Two_Sum/twoSum2.py
@@ -1,7 +1,7 @@
 def twoSum(nums,target):
     dict = {}
     for i in range(len(nums)):
-        dict.update({nums[i] : i})  #Generate the dictionary: each elements in [nums] as Key, and index as dictionay value
+        dict.update({nums[i] : i})  #Generate the dictionary: each elements in [nums] as Key, and index as dictionary value
     for j in range(len(nums)):
         diff = target - nums[j]
         if diff in dict and dict[diff] != j: # Remove the case of one number and its double as target
