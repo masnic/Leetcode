f79a61b9c0ac8de8506cb6feab3821d2d05fb4a5
masnic <47881310+masnic@users.noreply.github.com>
2021-12-12 17:40:00 -0500

    Update 001_TwoSum.py

--- 0001_Two_Sum/001_TwoSum.py
+++ 0001_Two_Sum/001_TwoSum.py
@@ -1,1 +1,17 @@
+class Solution(object):
+    def two_sum(self, nums,target):
+        dict = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in dic :
+                return[dict[complement], i]
 
+            dict[nums[i]] = i
+
+if __name__ == "__main__":
+    nums = [2, 7, 11, 15]
+    target = 9
+    assert (Solution().two_sum(nums, target) == [0, 1])
+    nums = [3, 2, 4]
+    target = 6
+    assert (Solution().two_sum(nums, target) == [1, 2])
